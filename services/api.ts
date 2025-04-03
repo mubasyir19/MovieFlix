37962b207211cf7963f55e264951cc699609326d
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-04-03 21:38:23 +0700

    first commit

---
+++ services/api.ts
@@ -0,0 +1,42 @@
+export const TMDB_CONFIG = {
+  BASE_URL: 'https://api.themoviedb.org/3',
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error('Failed to fetch movies', response.statusText);
+  }
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+// const url = 'https://api.themoviedb.org/3/authentication';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjdkNTdhZGE2MTk5NDYwOTc1MDQ4NGYzMWI4NTA5NSIsIm5iZiI6MTc0MzY4MDA4My4wNTEsInN1YiI6IjY3ZWU3MjUzMTVmNmJhODZmMWUxOTFlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iR1sTUNVggcZsfsoe7J4O2MZZnixl_DelOCwYuFLHFU'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
