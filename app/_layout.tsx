e00a2ea4c5374e9f9f0cc07ce6da97fdd4024046
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-04-04 16:01:02 +0700

    add: detail movie

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,21 @@ import { StatusBar } from 'react-native';
 export default function RootLayout() {
   return (
     <>
-      <StatusBar hidden={true} />
+      {/* <StatusBar hidden={true} /> */}
+
       <Stack>
-        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-        <Stack.Screen name='movie/[id]' options={{ headerShown: false }} />
+        <Stack.Screen
+          name='(tabs)'
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name='movies/[id]'
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack>
     </>
   );
